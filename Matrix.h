53d75666f1cbd6914fd348dbff8d0ac5952331a5
Bartlomiej Dlugosz <bd523@ic.ac.uk>
2024-10-23 16:28:48 +0100

    Feat: Added class declarations in header file

--- Matrix.h
+++ Matrix.h
@@ -12,11 +12,13 @@ public:
   Matrix(std::vector<int> list_rep, int m, int n);
 
   static Matrix multiply(Matrix, Matrix);
+  static Matrix threaded_multiply(const Matrix&, const Matrix&);
 
   void set(int m, int n, int val);
   int get(int m, int n);
 
 private:
+  static void thread_multiply(int thread_index, int total_threads, Matrix& res, const Matrix& matrix1, const Matrix& matrix2);
   /* Represents the matrix in the form {{1, 2, ...}, {1, 2, ...}}
    * where each outer array is a row and each inner array is a column */
   std::vector<std::vector<int>> matrix;
